799113597a014ecfb237db11b13adaa542bf01c1
Kevin-LB <le.breton.kevin@icloud.com>
2024-05-14 15:41:13 +0200

    dépot du code source

--- main.py
+++ main.py
@@ -40,4 +40,5 @@ async def message(ctx, agenda_message):
   for emoji in ["✅", "❌"]:
     await message.add_reaction(emoji)
 
-bot.run('MTIzOTE4NTg5ODY1NTA1NTg5Mg.GOysL-.r5P8PJqpyIZ3jy20C0z9bL6qyD2FG51WsSGpZ4')
+token = 'MTIzOTE4NTg5ODY1NTA1NTg5Mg.GOysL-.r5P8PJqpyIZ3jy20C0z9bL6qyD2FG51WsSGpZ4'
+bot.run(token)
